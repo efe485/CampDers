b2685d3186a7e258ad6d294852d854a1f154b53e
Efe Bilgin <bilginefe@outlook.com>
2021-01-23 20:35:18 +0300

    Non

--- Functions/SepetManager.cs
+++ Functions/SepetManager.cs
@@ -9,7 +9,7 @@ namespace Functions
         //convention
         public void Ekle(Urun urun) //neden burada class tipinden bir değer verdik. Buraya string urunAdi sekliden ekleme de yapabilirdik. Ama neden class tipinde bir parametre ekledik. Cünkü yönetim gelip bir parametre daha ekleyelim derse her yerde tek tek bu metodu cagırdığın yerlere gidip parametreleri vermek zorundasın. 
         {
-            Console.WriteLine("Sepete Eklendi : " + urun.urunAd);
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + urun.urunAd);
         }
     }
 }
