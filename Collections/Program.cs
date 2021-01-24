6ae95ae0e3070e61fcd49adcd7e2e6c7f7c3b618
Efe Bilgin <bilginefe@outlook.com>
2021-01-24 20:37:53 +0300

    24.01.2021

---
+++ Collections/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string>();
+            isimler.Add("Efe");
+            isimler.Add("Bilgin");
+            isimler.Remove("Efe");
+        }
+    }
+}
